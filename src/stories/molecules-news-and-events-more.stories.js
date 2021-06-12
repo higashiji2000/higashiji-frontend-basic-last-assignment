f348ba6f4889968e5043ffb4137de27afd709be6
Haruki Higashiji <h.higashiji@4nonome.com>
2021-06-12 20:55:17 +0900

    Create molecules-news-and-evens-more

---
+++ src/stories/molecules-news-and-events-more.stories.js
@@ -0,0 +1,6 @@
+import Molecules_News_and_events_more from "../components/molecules/news-and-events-more/news-and-events-more.html";
+import "../components/molecules/news-and-events-more/news-and-events-more.scss";
+
+export default { title: "Molecules/news-and-events/more" };
+
+export const News_and_events_more = () => Molecules_News_and_events_more;
