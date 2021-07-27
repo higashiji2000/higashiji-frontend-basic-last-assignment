e750c874d877d05d2578a7898b1dc572039bfbd3
Haruki Higashiji <h.higashiji@4nonome.com>
2021-07-27 13:15:40 +0900

    initial commit

---
+++ src/stories/organisms-events.stories.js
@@ -0,0 +1,6 @@
+import Organisms_Events from "../components/organisms/events/events.html";
+import "../components/organisms/events/events.scss";
+
+export default { title: "Organisms/events" };
+
+export const Events = () => Organisms_Events;
